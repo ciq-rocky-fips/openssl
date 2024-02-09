ad573402cda0d36637051e8a1ff89aebc71d2e42
rpm-build <rpm-build>
2024-02-09 14:00:37 -0500

    adding additional tests

--- FT.sh
+++ FT.sh
@@ -6,7 +6,7 @@
 declare -a array=("SHA1" "SHA2" "SHA3" "AES_GCM" "AES_ECB_Decrypt" "RSA" \
 "ECDSA" "TLS13_KDF_EXTRACT" "TLS13_KDF_EXPAND" "TLS12_PRF" \
 "PBKDF2" "SSHKDF" "KBKDF" "HKDF" "SSKDF" "X963KDF" "X942KDF" \
-"HASH" "CTR" "HMAC" "DH" "ECDH" "RSA_Encrypt" "RSA_Decrypt")
+"HASH" "CTR" "HMAC" "DH" "ECDH" "RSA_Encrypt" "RSA_Decrypt" "INTEGRITY" "RNG")
 
 echo "************************************"
 for i in ${!array[@]}; do    

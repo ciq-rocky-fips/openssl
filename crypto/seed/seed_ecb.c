fb0c374da0ffc16288368c7a620fcb53cb5ca7af
Jeremy Allison <jallison@ciq.com>
2024-07-22 15:38:01 -0700

    Disable low-level SEED algorithm functions in FIPS mode.
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- crypto/seed/seed_ecb.c
+++ crypto/seed/seed_ecb.c
@@ -7,11 +7,24 @@
  * https://www.openssl.org/source/license.html
  */
 
+# include "openssl/opensslconf.h"
+
+#ifdef OPENSSL_FIPS
+# include "openssl/fips.h"
+# include "openssl/err.h"
+#endif
+
 #include <openssl/seed.h>
 
 void SEED_ecb_encrypt(const unsigned char *in, unsigned char *out,
                       const SEED_KEY_SCHEDULE *ks, int enc)
 {
+    if (FIPS_mode()) {
+        FIPSerr(ERR_LIB_FIPS, FIPS_R_NON_FIPS_METHOD);
+        OpenSSLDie(__FILE__, __LINE__, "FATAL FIPS Unapproved algorithm called");
+        return;
+    }
+
     if (enc)
         SEED_encrypt(in, out, ks);
     else

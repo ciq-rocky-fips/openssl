6f9107bced1afa425043c3634d909d80950b3700
Jeremy Allison <jallison@ciq.com>
2024-07-23 11:44:06 -0700

    Disable low-level AES_ige_encrypt/AES_ige_decrypt functions in FIPS mode.
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- crypto/aes/aes_ige.c
+++ crypto/aes/aes_ige.c
@@ -7,6 +7,13 @@
  * https://www.openssl.org/source/license.html
  */
 
+# include "openssl/opensslconf.h"
+
+#ifdef OPENSSL_FIPS
+# include "openssl/fips.h"
+# include "openssl/err.h"
+#endif
+
 #include "internal/cryptlib.h"
 
 #include <openssl/aes.h>
@@ -44,6 +51,11 @@ void AES_ige_encrypt(const unsigned char *in, unsigned char *out,
 {
     size_t n;
     size_t len = length;
+
+    if (FIPS_mode()) {
+        FIPSerr(ERR_LIB_FIPS, FIPS_R_NON_FIPS_METHOD);
+        return;
+    }
 
     if (length == 0)
         return;
@@ -183,6 +195,11 @@ void AES_bi_ige_encrypt(const unsigned char *in, unsigned char *out,
     unsigned char prev[AES_BLOCK_SIZE];
     const unsigned char *iv;
     const unsigned char *iv2;
+
+    if (FIPS_mode()) {
+        FIPSerr(ERR_LIB_FIPS, FIPS_R_NON_FIPS_METHOD);
+        return;
+    }
 
     OPENSSL_assert(in && out && key && ivec);
     OPENSSL_assert((AES_ENCRYPT == enc) || (AES_DECRYPT == enc));

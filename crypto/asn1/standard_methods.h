7c2b679534baa33c730e73362369663d9c0469a6
Jeremy Allison <jallison@ciq.com>
2024-07-23 17:43:47 -0700

    Use a separate array "standard_methods_fips" for getting ASN1 key methods when in FIPS mode.
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- crypto/asn1/standard_methods.h
+++ crypto/asn1/standard_methods.h
@@ -59,3 +59,61 @@ static const EVP_PKEY_ASN1_METHOD *standard_methods[] = {
 #endif
 };
 
+static const EVP_PKEY_ASN1_METHOD *standard_methods_fips[] = {
+#ifndef OPENSSL_NO_RSA
+    &rsa_asn1_meths[0],
+    &rsa_asn1_meths[1],
+#endif
+#ifndef OPENSSL_NO_DH
+    &dh_asn1_meth,
+#endif
+#ifndef OPENSSL_NO_DSA
+#ifndef OPENSSL_FIPS
+    &dsa_asn1_meths[0],
+    &dsa_asn1_meths[1],
+    &dsa_asn1_meths[2],
+    &dsa_asn1_meths[3],
+    &dsa_asn1_meths[4],
+#endif
+#endif
+#ifndef OPENSSL_NO_EC
+    &eckey_asn1_meth,
+#endif
+    &hmac_asn1_meth,
+#ifndef OPENSSL_NO_CMAC
+    &cmac_asn1_meth,
+#endif
+#ifndef OPENSSL_NO_RSA
+    &rsa_pss_asn1_meth,
+#endif
+#ifndef OPENSSL_NO_DH
+    &dhx_asn1_meth,
+#endif
+#ifndef OPENSSL_NO_EC
+#ifndef OPENSSL_FIPS
+    &ecx25519_asn1_meth,
+    &ecx448_asn1_meth,
+#endif
+#endif
+#ifndef OPENSSL_NO_POLY1305
+#ifndef OPENSSL_FIPS
+    &poly1305_asn1_meth,
+#endif
+#endif
+#ifndef OPENSSL_NO_SIPHASH
+#ifndef OPENSSL_FIPS
+    &siphash_asn1_meth,
+#endif
+#endif
+#ifndef OPENSSL_NO_EC
+#ifndef OPENSSL_FIPS
+    &ed25519_asn1_meth,
+    &ed448_asn1_meth,
+#endif
+#endif
+#ifndef OPENSSL_NO_SM2
+#ifndef OPENSSL_FIPS
+    &sm2_asn1_meth,
+#endif
+#endif
+};

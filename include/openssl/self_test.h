806556e99d70c8f34c18cd54c23c837cfdad68b9
Jeremy Allison <jallison@ciq.com>
2024-10-31 15:16:41 -0700

    Add KAT and enable support for ED25519 and ED448 in the FIPS provider.
    Still need a PCT, but this works and is valgrind clean.
    
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

--- include/openssl/self_test.h
+++ include/openssl/self_test.h
@@ -72,6 +72,8 @@ extern "C" {
 # define OSSL_SELF_TEST_DESC_KDF_TLS13_EXTRACT  "TLS13_KDF_EXTRACT"
 # define OSSL_SELF_TEST_DESC_KDF_TLS13_EXPAND   "TLS13_KDF_EXPAND"
 # define OSSL_SELF_TEST_DESC_RNG            "RNG"
+# define OSSL_SELF_TEST_DESC_SIGN_ED25519   "ED25519"
+# define OSSL_SELF_TEST_DESC_SIGN_ED448     "ED448"
 
 void OSSL_SELF_TEST_set_callback(OSSL_LIB_CTX *libctx, OSSL_CALLBACK *cb,
                                  void *cbarg);

1a164a89d397fd2c10244bc8a1bf06f77f839b84
Jeremy Allison <jallison@ciq.com>
2024-12-09 13:25:36 -0800

    Add build script.
    Signed-off-by: Jeremy Allison <jallison@ciq.com>

---
+++ debug_fips.sh
@@ -0,0 +1,12 @@
+./Configure \
+        --prefix="`pwd`/INSTALLED" --openssldir="`pwd`/INSTALLED/pki/tls" enable-ec_nistp_64_gcc_128 \
+        --system-ciphers-file="`pwd`/INSTALLED/crypto-policies/back-ends/openssl.config" \
+        --debug zlib enable-camellia enable-seed enable-rfc3779 enable-sctp \
+        enable-cms enable-md2 enable-rc5 enable-ktls enable-fips \
+        no-mdc2 no-ec2m no-sm2 no-sm4 enable-buildtest-c++ \
+        shared linux-x86_64 -Wa,--noexecstack -Wa,--generate-missing-build-notes=yes \
+        '-DDEVRANDOM="\"/dev/urandom\"" -DROCKY_FIPS_NAME="\"Rocky Linux 9 - OpenSSL FIPS Provider\"" \
+        -DROCKY_FIPS_VERSION="\"Rocky9.20241015\""'\
+        -Wl,--allow-multiple-definition
+make all
+make install
